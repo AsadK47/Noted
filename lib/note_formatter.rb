7dae4dc11fa346ff9a32fd51e4422c9ce5fcfce2
Asad Khan <asad_khan47@hotmail.co.uk>
2019-02-04 12:33:59 +0000

    Added note formatter class which is isolated in tests

--- lib/note_formatter.rb
+++ lib/note_formatter.rb
@@ -1,2 +1,5 @@
 class NoteFormatter
+  def format(note)
+    "Title: #{note.title}\n#{note.body}"
+  end
 end
--- spec/note_formatter_spec.rb
+++ spec/note_formatter_spec.rb
@@ -1,5 +1,11 @@
 require 'note_formatter'
 
 describe NoteFormatter do
+  subject(:note_formatter) { described_class.new }
+  let(:note) { double :note, title: "Title", body: "body" }
 
+  it 'formats notes correctly' do
+    expect(note_formatter.format(note)).to eq("Title: #{note.title}\n"\
+    "#{note.body}")
+  end
 end
